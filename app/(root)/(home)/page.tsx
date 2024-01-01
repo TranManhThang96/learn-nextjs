67c456fd42cc98f0099138619935bca4ed99c5e5
TranManhThang <tranmanhthang96@gmail.com>
2024-01-01 21:31:10 +0700

    add auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="h1-bold">H1</h1>
-      <h2 className="h2-bold">H2</h2>
-      <h3 className="h3-bold">H3</h3>
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const AskQuestion = () => {
-  return (
-    <div>AskQuestion</div>
-  )
-}
+  return <div>AskQuestion</div>;
+};
 
-export default AskQuestion
+export default AskQuestion;
